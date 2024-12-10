1acf2d169fc4cc905c258c453dcbdefaeb5f2b99
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2024-12-10 15:35:22 +0100

    feat: user can write his own to-do and it will be saved in data.txt  4.2

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::{fs::File, io,};
+use std::io::Write;
 fn main() {
-    println!("Hello, world!");
+    let mut name = String::new();
+    println!("New to-do?");
+    io::stdin().read_line(&mut name).expect("Read line failed.");
+
+    let mut data_file = File::create("data.txt").expect("Cannot create file");
+    data_file.write(name.as_bytes()).expect("Cannot write in file");
 }
