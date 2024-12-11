ebe774e766b2d38bbbdf9b3ace1c44d024b15e8e
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2024-12-11 08:14:13 +0100

    feat: user can add more than 1 to-dos   4.3

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
-use std::{fs::File, io,};
+use std::fs::OpenOptions;
+use std::io;
 use std::io::Write;
 fn main() {
     let mut name = String::new();
     println!("New to-do?");
     io::stdin().read_line(&mut name).expect("Read line failed.");
 
-    let mut data_file = File::create("data.txt").expect("Cannot create file");
-    data_file.write(name.as_bytes()).expect("Cannot write in file");
+    let mut todo_file = OpenOptions::new()
+    .create(true)
+    .read(true)
+    .write(true)
+    .append(true)
+    .open("data.txt").expect("Cannot open file");
+    
+    todo_file.write_all(name.as_bytes()).expect("Cannot write in file");
 }
