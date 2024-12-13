31dd653f085c0313b4350047d83095436b325c00
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2024-12-13 16:15:38 +0100

    feat: user can use delete function directly from terminal   4.6

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,51 @@
-use serde::{Serialize, Deserialize};
+use clap::Parser;
+use core::option::Option;
+use serde::{Deserialize, Serialize};
 use std::fs::{read_to_string, File};
 use std::io::{self, Write};
-
 
 #[derive(Serialize, Deserialize)]
 struct Todo {
     name: String,
 }
 
-fn main() {              
-    
-    let mut todo = String::new();
-    println!("New to-do?");
-    io::stdin().read_line(&mut todo).expect("Read line failed.");
-    
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    delete: Option<usize>,
+}
+
+fn main() {
+    let args = Args::parse();
+
     let mut read_file: Vec<Todo> = match read_to_string("data.json") {
         Ok(data) => serde_json::from_str(&data).expect("Cannot deserialize file"),
         Err(_) => Vec::new(),
     };
 
-    let todo  = todo.trim();
-
-    if todo.contains("--delete") {
-        let line_number:usize = todo.split_whitespace().last().expect("Can't find line with this number").parse().unwrap();
-        read_file.remove(line_number - 1);
-    } else {
-        read_file.push(Todo { name: todo.to_string() });
-    };
+    match args.delete {
+        None => {
+            let mut todo = String::new();
+            println!("New to-do?");
+            io::stdin().read_line(&mut todo).expect("Read line failed.");
+            read_file.push(Todo {
+                name: todo.trim().to_string(),
+            });
+        }
+        Some(_todo_to_delete) => {
+            if read_file.len() < _todo_to_delete {
+                println!("Cannot delete line: line doesn't exist")
+            } else {
+                read_file.remove(_todo_to_delete - 1);
+            }
+        }
+    }
 
     let mut _data_file = File::create("data.json").expect("Cannot create file");
     let json_content = serde_json::to_string(&read_file).expect("Cannot write in file");
 
-    _data_file.write_all(json_content.as_bytes()).expect("Cannot write in file");
+    _data_file
+        .write_all(json_content.as_bytes())
+        .expect("Cannot write in file");
 }
